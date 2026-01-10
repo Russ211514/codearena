e5f2eeb7fdbcd81c971a2455ff8dbb1fd433e572
Russ211514 <russianjelomaster@gmail.com>
2026-01-10 14:25:17 +0800

    2nd commit

--- Scripts/language_selection.gd
+++ Scripts/language_selection.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Selection Scene.tscn")
+
+func _on_html_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Html tutorial.tscn")
